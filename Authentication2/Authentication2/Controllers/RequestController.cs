170e4304c760337be3ef9ec38f67d684c91f586f
Josh Thonnissen <jthonnis@emich.edu>
2019-02-19 20:05:10 -0500

    Create Layout

--- Authentication2/Authentication2/Controllers/RequestController.cs
+++ Authentication2/Authentication2/Controllers/RequestController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Authentication2.DataAccessLayer;
+using Authentication2.VIewModels;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -10,10 +12,35 @@ namespace Authentication2.Controllers
 {
     public class RequestController : Controller
     {
+        private readonly MyIdentityContext _context;
+        
+         public RequestController(MyIdentityContext context)
+        {
+            _context = context;
+        }
         // GET: /<controller>/
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateRequestViewModel model){
+
+                //TODO: create req model from the view model
+
+                // add to the context
+
+                // save
+
+//return RedirectToAction()
+                return Content("Post method create");
+
         }
     }
 }
