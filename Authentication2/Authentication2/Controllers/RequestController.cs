addcfadb6e27df6dafef8331b89d81268bff0fe8
MLGainz <majorleaguegains@gmail.com>
2019-03-01 00:23:50 -0500

    Open Request Page and Picking Up Requests!
    The open request page displays requests that do not have a driver yet (except requests made by the user logged in, you can't deliver to yourself) and gives the ability to pick them up. The page is only accessible by drivers.  Once a request has a driver it is no longer displayed on the page, the DriverId is set to the driver's UserId, and the status of the request changes to "Accepted By Driver".

--- Authentication2/Authentication2/Controllers/RequestController.cs
+++ Authentication2/Authentication2/Controllers/RequestController.cs
@@ -152,6 +152,9 @@ namespace Authentication2.Controllers
                 CreateRequestViewModel requestVM = new CreateRequestViewModel
                 {
                     Id = request.Id,
+                    UserId = request.UserId,
+                    DriverId = request.DriverId,
+                    Status = request.Status,
                     PickupStreetNumber = request.PickupAddress.StreetNumber,
                     PickupStreetName = request.PickupAddress.StreetName,
                     PickupCity = request.PickupAddress.City,
@@ -189,6 +192,9 @@ namespace Authentication2.Controllers
                 CreateRequestViewModel requestVM = new CreateRequestViewModel
                 {
                     Id = request.Id,
+                    UserId = request.UserId,
+                    DriverId = request.DriverId,
+                    Status = request.Status,
                     PickupStreetNumber = request.PickupAddress.StreetNumber,
                     PickupStreetName = request.PickupAddress.StreetName,
                     PickupCity = request.PickupAddress.City,
@@ -221,6 +227,9 @@ namespace Authentication2.Controllers
                     .Include(req => req.PickupAddress)
                     .FirstOrDefault();
 
+                existingRequest.UserId = request.UserId;
+                existingRequest.DriverId = request.DriverId;
+                existingRequest.Status = request.Status;
                 existingRequest.PickupAddress.StreetNumber = request.PickupStreetNumber;
                 existingRequest.PickupAddress.StreetName = request.PickupStreetName;
                 existingRequest.PickupAddress.City = request.PickupCity;
@@ -271,6 +280,112 @@ namespace Authentication2.Controllers
             }
 
             return Content("Please log in to use this feature");
+        }
+
+        public IActionResult OpenRequests()
+        {
+            if (User.Identity.IsAuthenticated && User.IsInRole("Driver"))
+            {
+                List<RequestModel> requests = _context.Requests
+                    .Include(req => req.DropOffAddress)
+                    .Include(req => req.PickupAddress)
+                    .ToList();
+
+                List<CreateRequestViewModel> requestsView = new List<CreateRequestViewModel> { };
+                foreach (RequestModel model in requests)
+                {
+                    if (model.DriverId == null && model.UserId != User.FindFirstValue(ClaimTypes.NameIdentifier))
+                        requestsView.Add(new CreateRequestViewModel(model));
+                }
+                return View(requestsView);
+            }
+
+            return Content("Please log in as a driver to use this feature");
+        }
+
+        public IActionResult PickupRequest(int? id)
+        {
+            if (User.Identity.IsAuthenticated && User.IsInRole("Driver"))
+            {
+                RequestModel request = _context.Requests
+                    .Where(r => r.Id == id)
+                    .Include(req => req.DropOffAddress)
+                    .Include(req => req.PickupAddress)
+                    .FirstOrDefault();
+
+                if (request == null)
+                    return Content("Request with given id does not exist.");
+
+                CreateRequestViewModel requestVM = new CreateRequestViewModel
+                {
+                    Id = request.Id,
+                    UserId = request.UserId,
+                    DriverId = request.DriverId,
+                    Status = request.Status,
+                    PickupStreetNumber = request.PickupAddress.StreetNumber,
+                    PickupStreetName = request.PickupAddress.StreetName,
+                    PickupCity = request.PickupAddress.City,
+                    PickupState = request.PickupAddress.State,
+                    PickupZipcode = request.PickupAddress.ZipCode,
+                    PickupInstructions = request.PickUpInstructions,
+                    DropoffStreetNumber = request.DropOffAddress.StreetNumber,
+                    DropoffStreetName = request.DropOffAddress.StreetName,
+                    DropoffCity = request.DropOffAddress.City,
+                    DropoffState = request.DropOffAddress.State,
+                    DropoffZipcode = request.DropOffAddress.ZipCode,
+                    DropoffInstructions = request.DropOffInstructions,
+                    Item = request.Item
+                };
+
+                return View(requestVM);
+            }
+
+            return Content("Please log in as a driver to use this feature");
+        }
+
+        [HttpPost]
+        public IActionResult PickupRequest(CreateRequestViewModel request)
+        {
+            if (User.Identity.IsAuthenticated && User.IsInRole("Driver"))
+            {
+                RequestModel existingRequest = _context.Requests
+                    .Where(r => r.Id == request.Id)
+                    .Include(req => req.DropOffAddress)
+                    .Include(req => req.PickupAddress)
+                    .FirstOrDefault();
+                
+                existingRequest.UserId = request.UserId;
+                existingRequest.DriverId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+                existingRequest.Status = "Accepted By Driver";
+                existingRequest.PickupAddress.StreetNumber = request.PickupStreetNumber;
+                existingRequest.PickupAddress.StreetName = request.PickupStreetName;
+                existingRequest.PickupAddress.City = request.PickupCity;
+                existingRequest.PickupAddress.State = request.PickupState;
+                existingRequest.PickupAddress.ZipCode = request.PickupZipcode;
+                existingRequest.DropOffAddress.StreetNumber = request.DropoffStreetNumber;
+                existingRequest.DropOffAddress.StreetName = request.DropoffStreetName;
+                existingRequest.DropOffAddress.City = request.DropoffCity;
+                existingRequest.DropOffAddress.State = request.DropoffState;
+                existingRequest.DropOffAddress.ZipCode = request.DropoffZipcode;
+                existingRequest.Item = request.Item;
+                existingRequest.PickUpInstructions = request.PickupInstructions;
+                existingRequest.DropOffInstructions = request.DropoffInstructions;
+
+                _context.Update<RequestModel>(existingRequest);
+                _context.SaveChanges();
+
+                return RedirectToAction("ConfirmPickup");
+            }
+
+            return Content("Please log in as driver to use this feature");
+        }
+
+        public IActionResult ConfirmPickup()
+        {
+            if (User.Identity.IsAuthenticated && User.IsInRole("Driver"))
+                return View();
+
+            return Content("Please log in as driver to use this feature");
         }
     }
 }
