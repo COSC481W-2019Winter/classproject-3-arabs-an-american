a052f31111c5e1c3dddd45d8490d63b7fe10fd53
MLGainz <majorleaguegains@gmail.com>
2019-03-20 01:03:24 -0400

    Fixed Address Overwriting
    Create method now will search for, and use an already existing address if it matches the input fields before it creates a new address. Update method no longer overwrites addresses and instead uses an already existing address or creates a new one. Both methods use user-specific addresses, meaning that an address may be in the database more than once but that is due to a different userId to associate it with their addressbook.

--- Authentication2/Authentication2/Areas/User/Controllers/RequestController.cs
+++ Authentication2/Authentication2/Areas/User/Controllers/RequestController.cs
@@ -54,28 +54,63 @@ namespace Authentication2.Areas.Controllers
                 {
                     UserId = User.FindFirstValue(ClaimTypes.NameIdentifier),
                     Status = "Awaiting Driver",
-                    PickupAddress = new Identity.Address
-                    {
-                        UserId = User.FindFirstValue(ClaimTypes.NameIdentifier),
-                        StreetNumber = model.PickupStreetNumber,
-                        StreetName = model.PickupStreetName,
-                        City = model.PickupCity,
-                        State = model.PickupState,
-                        ZipCode = model.PickupZipcode
-                    },
-                    DropOffAddress = new Identity.Address
-                    {
-                        UserId = User.FindFirstValue(ClaimTypes.NameIdentifier),
-                        StreetNumber = model.DropoffStreetNumber,
-                        StreetName = model.DropoffStreetName,
-                        City = model.DropoffCity,
-                        State = model.DropoffState,
-                        ZipCode = model.DropoffZipcode
-                    },
                     Item = model.Item,
                     PickUpInstructions = model.PickupInstructions,
                     DropOffInstructions = model.DropoffInstructions,
                 };
+
+                string number = model.PickupStreetNumber;
+                string name = model.PickupStreetName;
+                string city = model.PickupCity;
+                string state = model.PickupState;
+                int zip = model.PickupZipcode;
+
+                if (_context.Addresses.Any(x => x.UserId == request.UserId && x.StreetNumber == number && x.StreetName == name && x.City == city && x.State == state && x.ZipCode == zip))
+                {
+                    request.PickupAddressId = _context.Addresses
+                        .Where(x => x.StreetNumber == number && x.StreetName == name && x.City == city && x.State == state && x.ZipCode == zip)
+                        .FirstOrDefault()
+                        .Id;
+                }
+                else
+                {
+                    request.PickupAddress = new Identity.Address
+                    {
+                        UserId = request.UserId,
+                        StreetNumber = number,
+                        StreetName = name,
+                        City = city,
+                        State = state,
+                        ZipCode = zip
+                    };
+                }
+
+
+                number = model.DropoffStreetNumber;
+                name = model.DropoffStreetName;
+                city = model.DropoffCity;
+                state = model.DropoffState;
+                zip = model.DropoffZipcode;
+
+                if (_context.Addresses.Any(x => x.UserId == request.UserId && x.StreetNumber == number && x.StreetName == name && x.City == city && x.State == state && x.ZipCode == zip))
+                {
+                    request.DropOffAddressId = _context.Addresses
+                        .Where(x => x.StreetNumber == number && x.StreetName == name && x.City == city && x.State == state && x.ZipCode == zip)
+                        .FirstOrDefault()
+                        .Id;
+                }
+                else
+                {
+                    request.DropOffAddress = new Identity.Address
+                    {
+                        UserId = request.UserId,
+                        StreetNumber = number,
+                        StreetName = name,
+                        City = city,
+                        State = state,
+                        ZipCode = zip
+                    };
+                }
 
                 // add to the context
                 _context.Add(request);
@@ -216,16 +251,60 @@ namespace Authentication2.Areas.Controllers
                 existingRequest.UserId = request.UserId;
                 existingRequest.DriverId = request.DriverId;
                 existingRequest.Status = request.Status;
-                existingRequest.PickupAddress.StreetNumber = request.PickupStreetNumber;
-                existingRequest.PickupAddress.StreetName = request.PickupStreetName;
-                existingRequest.PickupAddress.City = request.PickupCity;
-                existingRequest.PickupAddress.State = request.PickupState;
-                existingRequest.PickupAddress.ZipCode = request.PickupZipcode;
-                existingRequest.DropOffAddress.StreetNumber = request.DropoffStreetNumber;
-                existingRequest.DropOffAddress.StreetName = request.DropoffStreetName;
-                existingRequest.DropOffAddress.City = request.DropoffCity;
-                existingRequest.DropOffAddress.State = request.DropoffState;
-                existingRequest.DropOffAddress.ZipCode = request.DropoffZipcode;
+
+                string number = request.PickupStreetNumber;
+                string name = request.PickupStreetName;
+                string city = request.PickupCity;
+                string state = request.PickupState;
+                int zip = request.PickupZipcode;
+
+                if(_context.Addresses.Any(x => x.UserId == request.UserId && x.StreetNumber == number && x.StreetName == name && x.City == city && x.State == state && x.ZipCode == zip))
+                {
+                    existingRequest.PickupAddressId = _context.Addresses
+                        .Where(x => x.StreetNumber == number && x.StreetName == name && x.City == city && x.State == state && x.ZipCode == zip)
+                        .FirstOrDefault()
+                        .Id;
+                }
+                else
+                {
+                    existingRequest.PickupAddress = new Identity.Address
+                    {
+                        UserId = request.UserId,
+                        StreetNumber = number,
+                        StreetName = name,
+                        City = city,
+                        State = state,
+                        ZipCode = zip
+                    };
+                }
+                
+
+                number = request.DropoffStreetNumber;
+                name = request.DropoffStreetName;
+                city = request.DropoffCity;
+                state = request.DropoffState;
+                zip = request.DropoffZipcode;
+
+                if (_context.Addresses.Any(x => x.UserId == request.UserId && x.StreetNumber == number && x.StreetName == name && x.City == city && x.State == state && x.ZipCode == zip))
+                {
+                    existingRequest.DropOffAddressId = _context.Addresses
+                        .Where(x => x.StreetNumber == number && x.StreetName == name && x.City == city && x.State == state && x.ZipCode == zip)
+                        .FirstOrDefault()
+                        .Id;
+                }
+                else
+                {
+                    existingRequest.DropOffAddress = new Identity.Address
+                    {
+                        UserId = request.UserId,
+                        StreetNumber = number,
+                        StreetName = name,
+                        City = city,
+                        State = state,
+                        ZipCode = zip
+                    };
+                }
+
                 existingRequest.Item = request.Item;
                 existingRequest.PickUpInstructions = request.PickupInstructions;
                 existingRequest.DropOffInstructions = request.DropoffInstructions;
