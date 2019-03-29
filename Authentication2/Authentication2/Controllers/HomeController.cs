7328703e6762c7475c9c51c9d9bb8cada5cbdb81
Josh <jthonnis@emich.edu>
2019-03-28 21:11:24 -0400

    About page

--- Authentication2/Authentication2/Controllers/HomeController.cs
+++ Authentication2/Authentication2/Controllers/HomeController.cs
@@ -29,6 +29,11 @@ namespace Authentication2.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
