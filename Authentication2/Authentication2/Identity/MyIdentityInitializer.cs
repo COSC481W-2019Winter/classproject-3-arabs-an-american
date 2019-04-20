d52767d6a74470d4b01ffbd4b3ef08ae7c36c265
Zafer Khourdaji <khourdaji@gmail.com>
2019-04-20 13:36:23 -0400

    Seed all users

--- Authentication2/Authentication2/Identity/MyIdentityInitializer.cs
+++ Authentication2/Authentication2/Identity/MyIdentityInitializer.cs
@@ -1,19 +1,25 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System.Diagnostics;
+using Authentication2.DataAccessLayer;
 
 namespace Authentication2.Identity
 {
     public class MyIdentityInitializer
     {
-        public static void SeedData(UserManager<MyIdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        public static void SeedData(
+        UserManager<MyIdentityUser> userManager, 
+            RoleManager<IdentityRole> roleManager,
+            MyProductionDbContext context
+            )
         {
             SeedRoles(roleManager);
             SeedUsers(userManager);
+            SeedRequests(context);
         }
 
         public static void SeedRoles(RoleManager<IdentityRole> roleManager)
         {
-       
+
             if (!roleManager.RoleExistsAsync("User").Result)
             {
                 IdentityResult result = roleManager.CreateAsync(new IdentityRole("User")).Result;
@@ -28,34 +34,30 @@ namespace Authentication2.Identity
         {
             if (userManager.FindByNameAsync("zafer").Result == null)
             {
-                MyIdentityUser zafer = new MyIdentityUser() {
+                MyIdentityUser zafer = new MyIdentityUser
+                {
                     UserName = "zafer",
                     Email = "khourdaji@gmail.com",
-                    Address = new Address {
+                    Address = new Address
+                    {
                         StreetName = "main st",
                         StreetNumber = "123",
                         City = "Ann Arbor",
                         State = "MI",
                         ZipCode = 48103
                     },
-                    Password = "zafer"
+                    Password = "zafer",
+                    PhoneNumber = "7349254343"
+
                 };
 
                 IdentityResult zaferResult = userManager.CreateAsync(zafer, zafer.Password).Result;
-                if (zaferResult.Succeeded)
-                {
-                    IdentityResult zaferRoleResult = userManager.AddToRoleAsync(zafer, "User").Result;
-                    if (zaferRoleResult.Succeeded)
-                    {
-                        Debug.WriteLine(zafer.UserName + " User created successfuly");
-                    }
-                }
+                IdentityResult zaferRoleResult = userManager.AddToRoleAsync(zafer, "User").Result;
 
-
-                MyIdentityUser anas = new MyIdentityUser()
+                MyIdentityUser anas = new MyIdentityUser
                 {
                     UserName = "anas",
-                    Email = "anas@gmail.com",
+                    Email = "anasmilhem1997@gmail.com",
                     Address = new Address
                     {
                         StreetName = "main st",
@@ -64,21 +66,119 @@ namespace Authentication2.Identity
                         State = "MI",
                         ZipCode = 48197
                     },
-                    Password = "anas"
+                    Password = "anas",
+                    CarMake = "Honda",
+                    CarYear = "1992",
+                    CarModel = "Toyota",
+                    CarColor = "Purple",
+                    DriversLicense = "K123456789",
+                    LicensePlate = "DQT1234",
+                    PhoneNumber = "7341234567"
                 };
 
                 IdentityResult anasResult = userManager.CreateAsync(anas, anas.Password).Result;
-                if (anasResult.Succeeded)
+                IdentityResult anasRoleResult = userManager.AddToRoleAsync(anas, "Driver").Result;
+
+                MyIdentityUser josh = new MyIdentityUser
                 {
-                    IdentityResult anasRoleResult = userManager.AddToRoleAsync(anas, "Driver").Result;
-                    if (anasRoleResult.Succeeded)
+                    UserName = "josh",
+                    Email = "josh@gmail.com",
+                    Address = new Address
                     {
-                        Debug.WriteLine(anas.UserName + " Driver created successfuly");
-                    }
-                }
+                        StreetName = "main st",
+                        StreetNumber = "123",
+                        City = "Ypsilanti",
+                        State = "MI",
+                        ZipCode = 48197
+                    },
+                    Password = "josh",
+                    PhoneNumber = "7341234567"
+                };
+
+                IdentityResult jsohResult = userManager.CreateAsync(josh, josh.Password).Result;
+                IdentityResult joshRoleResult = userManager.AddToRoleAsync(josh, "User").Result;
+
+
+                MyIdentityUser sean = new MyIdentityUser
+                {
+                    UserName = "sean",
+                    Email = "sean@gmail.com",
+                    Address = new Address
+                    {
+                        StreetName = "main st",
+                        StreetNumber = "123",
+                        City = "Ypsilanti",
+                        State = "MI",
+                        ZipCode = 48197
+                    },
+                    Password = "sean",
+                    CarMake = "Honda",
+                    CarYear = "2012",
+                    CarModel = "Accord",
+                    CarColor = "Blue",
+                    LicensePlate = "GHH123",
+                    DriversLicense = "K4423123",
+                    PhoneNumber = "7341234567"
+                };
+
+                IdentityResult seanResult = userManager.CreateAsync(sean, sean.Password).Result;
+                IdentityResult seanRoleResult = userManager.AddToRoleAsync(sean, "Driver").Result;
+
+                MyIdentityUser moe = new MyIdentityUser
+                {
+                    UserName = "mattar",
+                    Email = "Mmattar@emich.edu",
+                    Address = new Address
+                    {
+                        StreetName = "main st",
+                        StreetNumber = "123",
+                        City = "Dearborn",
+                        State = "MI",
+                        ZipCode = 48000
+                    },
+                    Password = "mattar",
+                    PhoneNumber = "7341234567"
+                };
+
+                IdentityResult moeResult = userManager.CreateAsync(moe, moe.Password).Result;
+                IdentityResult moeRoleResult = userManager.AddToRoleAsync(moe, "User").Result;
+
+
+                MyIdentityUser gavin = new MyIdentityUser
+                {
+                    UserName = "gavin",
+                    Email = "gmaierde@emich.edu",
+                    Address = new Address
+                    {
+                        StreetName = "main st",
+                        StreetNumber = "123",
+                        City = "Ann Arbor",
+                        State = "MI",
+                        ZipCode = 48193
+                    },
+                    Password = "gavin",
+                    CarYear = "3045",
+                    CarMake = "Tesla",
+                    CarColor = "Black",
+                    CarModel = "S3",
+                    LicensePlate = "RTG1234",
+                    DriversLicense = "K123123",
+                    PhoneNumber = "7341234567"
+                };
+
+                IdentityResult gavinResult = userManager.CreateAsync(gavin, gavin.Password).Result;
+                IdentityResult gavinRoleResult = userManager.AddToRoleAsync(gavin, "Driver").Result;
 
             }
+
+
+
         }
-        
+
+  
+        public static void SeedRequests(MyProductionDbContext context)
+        {
+
+        }
     }
 }
